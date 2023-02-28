0d2b4752281138b4a1effbd9d00b459b4f1265b8
JimmyKristensen <siswanto@live.dk>
2023-02-28 10:34:51 +0100

    uploade image to firebase storage

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View , Image} from 'react-native';
 import Addbar from './components/Addbar';
 import Backbutton from './components/Backbutton';
 import Header from './components/Header';
 import { ScreenType } from './constants/constants';
 import AddNoteScreen from './screens/AddNoteScreen';
 import AllNoteScreen from './screens/AllNoteScreen';
-import {database} from './config/Firebase';
-import {collection, addDoc, orderBy, query, onSnapshot} from 'firebase/firestore';
+import {database, storage} from './config/Firebase';
+import {collection, addDoc, query, onSnapshot} from 'firebase/firestore';
+import { async } from '@firebase/util';
+import {ref, uploadBytes} from 'firebase/storage'
+import * as ImagePicker from 'expo-image-picker'
 
 
 
 export default function App() {
+  /* Use state */
   const [screen, setScreen] = useState(ScreenType.home)
   const [notes, setNotes] = useState([])
+
+
+  /* Name of the collection on firebase */
   const fireNotes = 'notes';
   const fireNoteRef = collection(database,fireNotes)
   let content;
+
+  /* reads the note collection */
   const readDB = () => {
     const q = query(fireNoteRef, ref => ref.orderBy('createdAt', 'desc'));
+    /* New array to save the data in */
     const snapArrya = [];
+    /* Snapshit is an active listener to react to any changes to the query,
+       call the onSnapshot method with an event handler callback*/
     onSnapshot(q, snapshot => {
       snapshot.forEach(snap =>{
         snapArrya.push({
@@ -31,6 +43,41 @@ export default function App() {
       setNotes(snapArrya);
     })
    }
+   
+   const pickImage = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes : ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality:1,
+    })
+
+    if (!result.canceled) {
+        console.log(result);
+        const source = { uri: result.assets[0].uri };
+        uploadImage(source);
+    } else {
+        alert('No image selected');
+    }
+  };
+
+  const uploadImage = async (imageToUpload) => {
+    try {
+        const imageUri = imageToUpload.uri;
+        let imageName = imageUri.slice(-20);
+        const response = await fetch(imageUri)
+        const blob = await response.blob()
+        if (imageName.includes("/")) {
+          imageName = imageName.replaceAll("/", "");
+        };
+        const reference = ref(storage, imageName)
+        const result = await uploadBytes(reference, blob)
+    } catch (error) {
+        console.log(error);
+    }
+  }
+   
+
+  /* Navigation*/
   if(screen === ScreenType.addNote){
     content = <AddNoteScreen onSave={(data) => {
       addDoc(fireNoteRef, {
@@ -39,7 +86,8 @@ export default function App() {
     }}/>
   } else if(screen === ScreenType.home){
     content = <AllNoteScreen notes={notes}/>
-  }  
+  }
+
   
   return (
     <View style={styles.container}>
@@ -53,6 +101,7 @@ export default function App() {
           }}/>
       </View>
       <Button title='Show all note' onPress={readDB}></Button>
+      <Button title='Pick Image' onPress={pickImage}></Button>
       {content}
       <StatusBar style="auto" />
     </View>
